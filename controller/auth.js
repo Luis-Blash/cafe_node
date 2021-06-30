ba5b9e24c88d8a39c31ecd9b6d891aaa3b773e37
luis-blash <blash3g@gmail.com>
2021-06-29 19:57:18 -0500

    Autentificacion de google terminado.

--- controller/auth.js
+++ controller/auth.js
@@ -4,30 +4,30 @@ const bcryptjs = require('bcryptjs');
 const Usuario = require('../models/usuarios');
 // helpers
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async (req, res = response) => {
-    const {correo, password} = req.body;
-    try{
-        const usuario = await Usuario.findOne({correo});
-        console.log(usuario)
+    const { correo, password } = req.body;
+    try {
+        const usuario = await Usuario.findOne({ correo });
         // Verifacar si el Email existe
-        if(!usuario){
+        if (!usuario) {
             return res.status(400).json({
                 msg: "El usuario / Password no son correctos"
             })
         }
         // El usuario esta activo
-        if(!usuario.estado){
+        if (!usuario.estado) {
             return res.status(400).json({
                 msg: "El usuario no existe"
             })
         }
         // verificar contraseña -- comparamos si es la misma
         const validPassword = bcryptjs.compareSync(password, usuario.password)
-        if(!validPassword){
+        if (!validPassword) {
             return res.status(400).json({
-                msg: "El usuario / Password no son correctos" 
+                msg: "El usuario / Password no son correctos"
             })
         }
         // Generar JWT
@@ -37,21 +37,57 @@ const login = async (req, res = response) => {
             usuario,
             token
         })
-    }catch(error){
+    } catch (error) {
         console.log(error);
         res.status(500).json({
-            msg:"Hable con el administrador"
+            msg: "Hable con el administrador"
         })
     }
 }
 
-const googleSigning = (req, res = response) =>{
+// Sirve para hacer login y signing
+const googleSigning = async (req, res = response) => {
 
-    const {id_token} = req.body;
-    res.json({
-        msg: 'Google Signing',
-        id_token
-    })
+    const { id_token } = req.body;
+
+    try {
+        const { nombre, img, correo } = await googleVerify(id_token);
+
+        // Existe el correo en la base
+        let usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            // guardar
+            const data = {
+                nombre,
+                correo,
+                password: 'p',
+                img,
+                google: true
+            };
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // Si el usario de google esta en false
+        if (!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Usuario bloqueado hable con el administrador'
+            })
+        }
+
+        // Generar el jwt
+        const token = await generarJWT(usuario.id);
+
+        res.json({
+            usuario,
+            token
+        })
+
+    } catch (error) {
+        res.json({
+            msg: 'Token no es reconocido de Google'
+        })
+    }
 }
 
 module.exports = {
